f34673bb9f02ca7738bd25c01c0b6731cae79f04
Emiliano Monson <emimonson8gmail.com>
2026-04-23 13:56:54 -0300

    Cambios, arreglos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import * as AOS from 'aos';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,25 +7,6 @@ import * as AOS from 'aos';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class App implements AfterViewInit {
+export class App {
   title = 'nja-tech-landing';
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  // Cambiamos ngOnInit por ngAfterViewInit para asegurar que el HTML ya se dibujó
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      // Le damos un respiro mínimo a Angular para que acomode las imágenes
-      setTimeout(() => {
-        AOS.init({
-          duration: 600, // Lo bajamos de 800 a 600 para que sea más veloz
-          once: true,
-          offset: 30     // Arranca casi apenas aparece en pantalla (estaba en 100)
-        });
-        
-        // Forzamos a AOS a recalcular las posiciones exactas de todo
-        AOS.refresh();
-      }, 150);
-    }
-  }
 }
